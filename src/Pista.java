6ba89d919741db89e7b637e9e71bedfe0fd7bd00
Controle de Gastos <rod.frnnds@gmail.com>
2023-12-20 03:21:53 -0300

    Project Run

--- src/Pista.java
+++ src/Pista.java
@@ -2,8 +2,11 @@ import java.awt.Graphics;
 import java.awt.Color;
 public class Pista {
     public void render(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.fillRect(0,0,500,500);
         g.setColor(Color.gray);
         g.fillRect(150,0,200,500);
+        
     }
     
 }
